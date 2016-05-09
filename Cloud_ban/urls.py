09b344d2bd9afcae3af591bbd0ba9293d68cc960
Ahsanul08 <ahsanul.haque.ovi@gmail.com>
2016-05-09 14:52:03 +0600

    Ban request working in current version.

--- Cloud_ban/urls.py
+++ Cloud_ban/urls.py
@@ -5,7 +5,9 @@ from validation_app import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^signup/$', views.show_signup_page, name="sign_up"),
-    url(r'^log_in/$', views.do_signup, name="sign_up_form"),
     url(r'^log_in/$', views.show_login_page, name="log_in"),
-    url(r'^profile/$', views.show_profile, name="profile")
+    url(r'^log_in/$', views.do_signup, name="sign_up_form"),
+    url(r'^profile/$', views.show_profile, name="profile"),
+    url(r'^ban_report/$',views.make_ban_request, name = "ban_url"),
+    url(r'^log_in/$', views.logout, name= 'back_to_login')
 ]
--- validation_app/views.py
+++ validation_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from validation_app.models import User
 from django.db import IntegrityError
 from validation_app.models import Acl
+from subprocess import call
 
 # Create your views here.
 
@@ -29,14 +30,38 @@ def do_signup(request):
 
 def show_profile(request):
     user = User.objects.get(user_name=request.POST.get('username'))
+    request.META['USERNAME'] = user.user_name
     if user.password == request.POST.get('password'):
         if set_session(request, user.id):
-            return render(request, 'html/profile.html',{'user_name' : user.user_name})
+            acl_access = Acl.objects.get(id = user.acl_id_id).category
+            if acl_access == 'sticker_admin':
+                return render(request, 'html/sticker_admin_profile.html')
+            elif acl_access == 'web_admin':
+                return render(request, 'html/web_admin_profile.html')
         else:
             return HttpResponse("Couldn't login for some unknown event")
     else:
         return HttpResponse("Your username and password didn't match.")
 
+
+def make_ban_request(request):
+    ban_seeked_content = request.POST.getlist('checks[]')
+    print ban_seeked_content
+    #u_id = User.objects.get(user_name=request.META.get('USERNAME')).id
+    #if get_session(request,u_id):
+
+
+    """
+    host = ''
+
+    if User.objects.get(user_name=username).acl_id == 'sticker_admin':
+        host = 'http://ipvcloud.ringid.com'
+    elif User.objects.get(user_name=username).acl_id == 'web_admin':
+        host = 'http'
+    """
+    for individual_content in ban_seeked_content:
+        call("curl -X BAN http://ipvcloud.ringid.com/{0}".format(individual_content),shell= True)
+    return render(request,'html/form_output.html',{'ban_list': ban_seeked_content})
 
 def show_login_page(request):
     return render(request, 'html/login_form.html')
@@ -48,18 +73,18 @@ def show_signup_page(request):
 
 def logout(request):
     try:
-        del request.session['session_id']
+        del request.session['SESSION_ID']
     except KeyError:
         pass
     return render(request, 'html/login_form.html')
 
 
 def get_session(request,username):
-    return request.session['session_id'] == User.objects.get(user_name = username).id
+    return request.session['SESSION_ID'] == User.objects.get(user_name = username).id
 
 def set_session(request,u_id):
     try:
-        request.session['session_id'] = u_id
+        request.session['SESSION_ID'] = u_id
     except:
         return False
     return True
